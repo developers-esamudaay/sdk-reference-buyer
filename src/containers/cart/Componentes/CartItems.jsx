3f2a2e3d7104171941ff5afb2ebe35ac68134b59
Himanshu Gupta <hgupta2363@gmail.com>
2022-11-09 16:19:27 +0530

    design-improvments

--- src/containers/cart/Componentes/CartItems.jsx
+++ src/containers/cart/Componentes/CartItems.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { CartContext } from '../../../contextProviders/cartContextProvider'
 import CartItemsStyles from '../../../styles/cart/cartItemsOrderSummary.module.scss'
 import CrossIcon from '../../../shared/svg/cross-icon'
 import { ONDC_COLORS } from '../../../shared/colors'
+import { buttonTypes } from '../../../shared/button/utils'
+
+import Button from '../../../shared/button/button'
 import ProductCard from '../../ProductListing/Components/ProductCard'
 const CartItems = ({ onClose }) => {
   console.log('in cart Items')
-  const { cartItems } = useContext(CartContext)
+  const { cartItems, setCartItems } = useContext(CartContext)
+  const history = useHistory()
   console.log(cartItems)
   return (
     <div className={CartItemsStyles.cart_items_order_summary_wrapper}>
@@ -31,6 +36,34 @@ const CartItems = ({ onClose }) => {
             </div>
           )
         })}
+      </div>
+      <hr />
+      <div className={CartItemsStyles.itemCount}>
+        <p className={CartItemsStyles.label1}>Total Items</p>
+        <p className={CartItemsStyles.label1}>{cartItems.length}</p>
+      </div>
+      <div className={CartItemsStyles.itemCount}>
+        <p className={CartItemsStyles.label1}>Total Amount </p>
+        <p className={CartItemsStyles.label1}>{300}</p>
+      </div>
+      <hr />
+      <div className={CartItemsStyles.bottomButtons}>
+        <div className="pe-3">
+          <Button
+            button_type={buttonTypes.secondary}
+            button_hover_type={buttonTypes.secondary_hover}
+            button_text="Clear Cart"
+            onClick={() => setCartItems([])}
+          />
+        </div>
+        <div className="pe-3">
+          <Button
+            button_type={buttonTypes.primary}
+            button_hover_type={buttonTypes.primary_hover}
+            button_text="Checkout"
+            onClick={() => history.push('/application/initialize')}
+          />
+        </div>
       </div>
     </div>
   )
--- src/containers/cart/Componentes/OrderSummary.jsx
+++ src/containers/cart/Componentes/OrderSummary.jsx
@@ -28,75 +28,78 @@ export default function OrderSummary() {
   const [toggleCollapse, setToggleCollapse] = useState(false)
   return (
     <Fragment>
-      <div className={`${styles.order_summary_background}`}>
-        <div className="container h-100">
-          <div className="row align-items-center h-100">
-            <div className="col-xl-8 col-md-6 d-flex align-items-center">
-              <div className="px-1">
-                <div
-                  className={styles.collapse_button_wrapper}
-                  onClick={() => setToggleCollapse(!toggleCollapse)}
-                >
-                  {toggleCollapse ? (
-                    <div
-                      style={{
-                        transform: 'rotate(180deg)',
-                        transition: 'transform 0.7s',
-                      }}
-                    >
-                      <DropdownSvg width="15" height="15" color={ONDC_COLORS.WHITE} />
-                    </div>
-                  ) : (
-                    <div
-                      style={{
-                        transform: 'rotate(0)',
-                        transition: 'transform 0.7s',
-                      }}
-                    >
-                      <DropdownSvg width="15" height="15" color={ONDC_COLORS.WHITE} />
-                    </div>
-                  )}
+      {toggleCollapse ? (
+        <CartItems onClose={() => setToggleCollapse(!toggleCollapse)} />
+      ) : (
+        <div className={`${styles.order_summary_background}`}>
+          <div className="container h-100">
+            <div className="row align-items-center h-100">
+              <div className="col-xl-8 col-md-6 d-flex align-items-center">
+                <div className="px-1">
+                  <div
+                    className={styles.collapse_button_wrapper}
+                    onClick={() => setToggleCollapse(!toggleCollapse)}
+                  >
+                    {toggleCollapse ? (
+                      <div
+                        style={{
+                          transform: 'rotate(180deg)',
+                          transition: 'transform 0.7s',
+                        }}
+                      >
+                        <DropdownSvg width="15" height="15" color={ONDC_COLORS.WHITE} />
+                      </div>
+                    ) : (
+                      <div
+                        style={{
+                          transform: 'rotate(0)',
+                          transition: 'transform 0.7s',
+                        }}
+                      >
+                        <DropdownSvg width="15" height="15" color={ONDC_COLORS.WHITE} />
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="px-2">
+                  <p className={styles.total_items_in_cart_text}>
+                    Items in cart ({cartItems.length})
+                  </p>
+                </div>
+                <div className="ms-auto">
+                  <p className={styles.sub_total_text}>
+                    SubTotal:
+                    <span className="ps-2 pe-1">
+                      <IndianRupee height="13" width="8" color={ONDC_COLORS.PRIMARYCOLOR} />
+                    </span>
+                    <span>{getSubTotal(cartItems)}</span>
+                  </p>
                 </div>
               </div>
-              <div className="px-2">
-                <p className={styles.total_items_in_cart_text}>
-                  Items in cart ({cartItems.length})
-                </p>
-              </div>
-              <div className="ms-auto">
-                <p className={styles.sub_total_text}>
-                  SubTotal:
-                  <span className="ps-2 pe-1">
-                    <IndianRupee height="13" width="8" color={ONDC_COLORS.PRIMARYCOLOR} />
-                  </span>
-                  <span>{getSubTotal(cartItems)}</span>
-                </p>
-              </div>
-            </div>
-            <div
-              className={`col-xl-4 col-md-6 d-flex align-items-center ${styles.checkout_button_placement}`}
-            >
-              <div className="pe-3">
-                <Button
-                  button_type={buttonTypes.secondary}
-                  button_hover_type={buttonTypes.secondary_hover}
-                  button_text="Clear Cart"
-                  onClick={() => setCartItems([])}
-                />
-              </div>
-              <div className="pe-3">
-                <Button
-                  button_type={buttonTypes.primary}
-                  button_hover_type={buttonTypes.primary_hover}
-                  button_text="Checkout"
-                  onClick={() => history.push('/application/initialize')}
-                />
+              <div
+                className={`col-xl-4 col-md-6 d-flex align-items-center ${styles.checkout_button_placement}`}
+              >
+                <div className="pe-3">
+                  <Button
+                    button_type={buttonTypes.secondary}
+                    button_hover_type={buttonTypes.secondary_hover}
+                    button_text="Clear Cart"
+                    onClick={() => setCartItems([])}
+                  />
+                </div>
+                <div className="pe-3">
+                  <Button
+                    button_type={buttonTypes.primary}
+                    button_hover_type={buttonTypes.primary_hover}
+                    button_text="Checkout"
+                    onClick={() => history.push('/application/initialize')}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {toggleCollapse && <CartItems onClose={() => setToggleCollapse(!toggleCollapse)} />}
+      )}
     </Fragment>
   )
 }
