c0d348edb633a6071c376e5d3384b9ac46dfb0c8
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2023-02-23 20:11:48 +0530

    temp-changes

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -12,6 +12,6 @@ export const Urls: any = {
   findLatLngUrl: (areaCode: String, door: String) =>
     `search?key=${process.env.REACT_APP_LOCATION_IQ_KEY}& q=${door},${areaCode}&format=json`,
   locationSuggetionUrl: (query: String) =>
-    `/autocomplete?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${query}&format=json&countrycodes=in&limit=5`,
-  findAddressUrl:(loc:Location)=>`/reverse?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&lat=${loc?.lat}&lon=${loc?.lon}&format=json`
+    `/autocomplete?key=${process.env.REACT_APP_LOCATION_IQ_KEY??"pk.5f5daaeffc2eb3d8822a194dd499df69"}&q=${query}&format=json&countrycodes=in&limit=5`,
+  findAddressUrl:(loc:Location)=>`/reverse?key=${process.env.REACT_APP_LOCATION_IQ_KEY??"pk.5f5daaeffc2eb3d8822a194dd499df69"}&lat=${loc?.lat}&lon=${loc?.lon}&format=json`
 };
